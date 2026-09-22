ecfbe5c50127c65b860903b83bc90f47ed8b9d59
Julian <113358009+skywqlkered@users.noreply.github.com>
2026-09-22 16:49:39 +0200

    vector made for fildes[2];
    stuck at pipethatshit arguments/input

--- shell.cpp
+++ shell.cpp
@@ -1,12 +1,12 @@
 /**
-	* Shell framework
-	* course Operating Systems
-	* Radboud University
-	* v22.09.05
+  * Shell framework
+  * course Operating Systems
+  * Radboud University
+  * v22.09.05
 
-	Student names:
-	- ...
-	- ...
+  Student names:
+  - ...
+  - ...
 */
 
 /**
@@ -38,11 +38,13 @@
 // although it is good habit, you don't have to type 'std' before many objects by including this line
 using namespace std;
 
-struct Command {
+struct Command
+{
   vector<string> parts = {};
 };
 
-struct Expression {
+struct Expression
+{
   vector<Command> commands;
   string inputFromFile;
   string outputToFile;
@@ -50,35 +52,40 @@ struct Expression {
 };
 
 // Parses a string to form a vector of arguments. The separator is a space char (' ').
-vector<string> split_string(const string& str, char delimiter = ' ') {
+vector<string> split_string(const string &str, char delimiter = ' ')
+{
   vector<string> retval;
-  for (size_t pos = 0; pos < str.length(); ) {
+  for (size_t pos = 0; pos < str.length();)
+  {
     // look for the next space
     size_t found = str.find(delimiter, pos);
     // if no space was found, this is the last word
-    if (found == string::npos) {
+    if (found == string::npos)
+    {
       retval.push_back(str.substr(pos));
       break;
     }
     // filter out consequetive spaces
     if (found != pos)
-      retval.push_back(str.substr(pos, found-pos));
-    pos = found+1;
+      retval.push_back(str.substr(pos, found - pos));
+    pos = found + 1;
   }
   return retval;
 }
 // wrapper around the C execvp so it can be called with C++ strings (easier to work with)
 // always start with the command itself
 // DO NOT CHANGE THIS FUNCTION UNDER ANY CIRCUMSTANCE
-int execvp(const vector<string>& args) {
+int execvp(const vector<string> &args)
+{
   // build argument list
-  const char** c_args = new const char*[args.size()+1];
-  for (size_t i = 0; i < args.size(); ++i) {
+  const char **c_args = new const char *[args.size() + 1];
+  for (size_t i = 0; i < args.size(); ++i)
+  {
     c_args[i] = args[i].c_str();
   }
   c_args[args.size()] = nullptr;
   // replace current process with new process as specified
-  int rc = ::execvp(c_args[0], const_cast<char**>(c_args));
+  int rc = ::execvp(c_args[0], const_cast<char **>(c_args));
   // if we got this far, there must be an error
   int error = errno;
   // in case of failure, clean up memory (this won't overwrite errno normally, but let's be sure)
@@ -88,8 +95,9 @@ int execvp(const vector<string>& args) {
 }
 
 // Executes a command with arguments. In case of failure, returns error code.
-int execute_command(const Command& cmd) {
-  auto& parts = cmd.parts;
+int execute_command(const Command &cmd)
+{
+  auto &parts = cmd.parts;
   if (parts.size() == 0)
     return EINVAL;
 
@@ -98,18 +106,22 @@ int execute_command(const Command& cmd) {
   return retval ? errno : 0;
 }
 
-void display_prompt() {
+void display_prompt()
+{
   char buffer[512];
-  char* dir = getcwd(buffer, sizeof(buffer));
-  if (dir) {
+  char *dir = getcwd(buffer, sizeof(buffer));
+  if (dir)
+  {
     cout << "\e[32m" << dir << "\e[39m"; // the strings starting with '\e' are escape codes, that the terminal application interpets in this case as "set color to green"/"set color to default"
   }
   cout << "$ ";
   flush(cout);
 }
 
-string request_command_line(bool showPrompt) {
-  if (showPrompt) {
+string request_command_line(bool showPrompt)
+{
+  if (showPrompt)
+  {
     display_prompt();
   }
   string retval;
@@ -121,73 +133,75 @@ string request_command_line(bool showPrompt) {
 // Here, the user input can be parsed using the following approach.
 // First, divide the input into the distinct commands (as they can be chained, separated by `|`).
 // Next, these commands are parsed separately. The first command is checked for the `<` operator, and the last command for the `>` operator.
-Expression parse_command_line(string commandLine) {
+Expression parse_command_line(string commandLine)
+{
   Expression expression;
   vector<string> commands = split_string(commandLine, '|');
-  for (size_t i = 0; i < commands.size(); ++i) {
-    string& line = commands[i];
+  for (size_t i = 0; i < commands.size(); ++i)
+  {
+    string &line = commands[i];
     vector<string> args = split_string(line, ' ');
-    if (i == commands.size() - 1 && args.size() > 1 && args[args.size()-1] == "&") {
+    if (i == commands.size() - 1 && args.size() > 1 && args[args.size() - 1] == "&")
+    {
       expression.background = true;
-      args.resize(args.size()-1);
+      args.resize(args.size() - 1);
     }
-    if (i == commands.size() - 1 && args.size() > 2 && args[args.size()-2] == ">") {
-      expression.outputToFile = args[args.size()-1];
-      args.resize(args.size()-2);
+    if (i == commands.size() - 1 && args.size() > 2 && args[args.size() - 2] == ">")
+    {
+      expression.outputToFile = args[args.size() - 1];
+      args.resize(args.size() - 2);
     }
-    if (i == 0 && args.size() > 2 && args[args.size()-2] == "<") {
-      expression.inputFromFile = args[args.size()-1];
-      args.resize(args.size()-2);
+    if (i == 0 && args.size() > 2 && args[args.size() - 2] == "<")
+    {
+      expression.inputFromFile = args[args.size() - 1];
+      args.resize(args.size() - 2);
     }
     expression.commands.push_back({args});
   }
   return expression;
 }
 
-
-
 // framework for executing "date | tail -c 5" using raw commands
 // two processes are created, and connected to each other
-int pipethatshit(Command cmd1, Command cmd2) {
+int pipethatshit(vector<int*> fildes, Command cmd1, Command cmd2)
+{
   // create communication channel shared between the two processes
   // ...
-  int fildes[2];
-  
-  if (pipe(fildes) < 0) {
-    return 1;
-  }
 
   pid_t child1 = fork();
-  if (child1 == 0) {
+  if (child1 == 0)
+  {
     // redirect standard output (STDOUT_FILENO) to the input of the shared communication channel
     // free non used resources (why?)
 
     dup2(fildes[1], STDOUT_FILENO);
 
-    close(fildes[0]); close(fildes[1]); 
+    close(fildes[0]);
+    close(fildes[1]);
     execute_command(cmd1);
     printf("uhoh");
     // display nice warning that the executable could not be found
     abort(); // if the executable is not found, we should abort. (why?)
   }
 
-
-  
   pid_t child2 = fork();
-  if (child2 == 0) {
+  if (child2 == 0)
+  {
     // redirect the output of the shared communication channel to the standard input (STDIN_FILENO).
     // free non used resources (why?)
-    
+
     dup2(fildes[0], STDIN_FILENO);
 
-    close(fildes[0]); close(fildes[1]); 
+    close(fildes[0]);
+    close(fildes[1]);
     execute_command(cmd2);
     printf("uhoh");
     abort(); // if the executable is not found, we should abort. (why?)
   }
 
-  close(fildes[0]); close(fildes[1]); 
-  
+  close(fildes[0]);
+  close(fildes[1]);
+
   // free non used resources (why?)
   // wait on child processes to finish (why both?)
   waitpid(child1, nullptr, 0);
@@ -195,13 +209,32 @@ int pipethatshit(Command cmd1, Command cmd2) {
   return 0;
 }
 
-int execute_expression(Expression& expression) {
+vector<int*> make_pipez(int commandamount)
+{
+  vector<int*> filedes_arr;
+
+  for (int i = 0; i < commandamount - 1; i++)
+  {
+    int fildes[2];
+    filedes_arr[i] = fildes;
+
+    if (pipe(fildes) < 0)
+    {
+      vector<int *> empty_vec = {0x00};
+      return empty_vec;
+    }
+  }
+  return filedes_arr;
+}
+
+int execute_expression(Expression &expression)
+{
   // Check for empty expression
   if (expression.commands.size() == 0)
     return EINVAL;
 
   // Handle intern commands (like 'cd' and 'exit')
-  
+
   // External commands, executed with fork():
   // Loop over all commandos, and connect the output and input of the forked processes
 
@@ -210,21 +243,27 @@ int execute_expression(Expression& expression) {
 
   int commandamount = expression.commands.size();
 
-  for (int i = 0; i < commandamount-1; i++){
-    cout << i << endl;
-    pipethatshit(expression.commands[i], expression.commands[i+1]);
+  vector<int*> filedes_vector = make_pipez(commandamount);
+
+  for (int i = 0; i < (sizeof(filedes_vector) / sizeof(filedes_vector[0])); i++)
+  {
+    cout << i << "' memory addrress: " << &filedes_vector[i] << endl;
   }
 
+  for (int i = 0; i < commandamount - 1; i++)
+  {
+    pipethatshit(filedes_vector[i], expression.commands[i], expression.commands[i + 1]);
+  }
   return 0;
 }
 
-
-int shell(bool showPrompt) {
+int shell(bool showPrompt)
+{
   //* <- remove one '/' in front of the other '/' to switch from the normal code to step1 code
-  while (cin.good()) {
+  while (cin.good())
+  {
     string commandLine = request_command_line(showPrompt);
     Expression expression = parse_command_line(commandLine);
-
 
     int rc = execute_expression(expression);
     if (rc != 0)
@@ -232,11 +271,11 @@ int shell(bool showPrompt) {
   }
   return 0;
   /*/
-  
+
   string mystring1 = "date";
   string mystring2 = "tail -c 5";
 
-  
+
   vector<string> command = split_string(mystring1, ' ');
   vector<string> args2 = split_string(mystring2, ' ');
 
